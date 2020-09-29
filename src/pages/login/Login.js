46839f3c4ee4b9ce8056e6e7e59726b7c6b57e7e
Alex <alex.cessinas@erudo.fr>
2020-09-29 13:59:36 +0200

    :construction:

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -4,7 +4,24 @@ import React from 'react';
 function Login() {
   return (
     <div className="Settings">
-        Login
+        <div className="main">
+            <div className="col-md-6 col-sm-12">
+                <div className="login-form">
+                    <form>
+                        <div className="form-group">
+                            <label>User Name</label>
+                            <input type="text" className="form-control" placeholder="User Name">
+                        </div>
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input type="password" className="form-control" placeholder="Password">
+                        </div>
+                        <button type="submit" className="btn btn-black">Login</button>
+                        <button type="submit" className="btn btn-secondary">Register</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
