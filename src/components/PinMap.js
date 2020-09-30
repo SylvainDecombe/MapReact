e21ba80b13299e6b803136d26c108013d7d89506
Sylvain-Decombe <sylvain.decombe@etu.unilim.fr>
2020-09-30 15:52:58 +0200

    ListMarkers ok

--- src/components/PinMap.js
+++ src/components/PinMap.js
@@ -4,45 +4,43 @@ import Map from "react-leaflet/lib/Map";
 import Popup from "react-leaflet/lib/Popup";
 import Marker from "react-leaflet/lib/Marker";
 import Lefleat, { popup } from "leaflet";
+import 'leaflet/dist/leaflet.css'
 
-const MyPopupMarker = ({ content, position }: Props) => (
-  <Marker position={position}>
+const MyPopupMarker = ({ content, position }) => (
+  <Marker position={position} icon={iconMarker}>
     <Popup>{content}</Popup>
   </Marker>
 )
 
-const MyMarkersList = ({ markers }: { markers: Array<MarkerData> }) => {
+const MyMarkersList = ( {markers} ) => {
   const items = markers.map(({ key, ...props }) => (
     <MyPopupMarker key={key} {...props} />
   ))
   return <Fragment>{items}</Fragment>
 }
 
-var myIcon = Lefleat.icon({
-    iconUrl: 'https://static.wixstatic.com/media/2cd43b_73cacdc0c2434cfe8a83b5bca295e440~mv2.png/v1/fill/w_614,h_614,fp_0.50_0.50,lg_1/2cd43b_73cacdc0c2434cfe8a83b5bca295e440~mv2.png',
-    iconSize: [38,29],
+var iconMarker = Lefleat.icon({
+    iconUrl: 'https://www.iconpacks.net/icons/2/free-pine-icon-1577-thumb.png',
+    iconSize: [38, 95], // size of the icon
 });
 
 export class PinMap extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            /** Tableau des markers*/
             markers: [
-                { key: 'marker1', position: [45.65, 0.20], content: 'My first popup' },
-                { key: 'marker2', position: [45.65, 0.35], content: 'My second popup' },
-                { key: 'marker3', position: [45.65, 0.5], content: 'My third popup' },
+                { key: 'marker1', position: [45.65, 0.15], content: 'My first popup' },
+                { key: 'marker2', position: [45.65, 0.18], content: 'My second popup' },
+                { key: 'marker3', position: [45.65, 0.20], content: 'My third popup' },
             ],
         }
     }
 
     render() {
         return ( 
-            <Map style = { { height: '800px', width: '100%' } } center = { [45.65, 0.15] } zoom = { 13 } >
+            <Map style = { { height: '800px', width: '100%' } } center = { [45.65, 0.15] } zoom = { 13 } onClick={this.addMarker}>
                 <TileLayer attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url = 'https://{s}.tile.osm.org/{z}/{x}/{y}.png' />
-                <Marker position={ [45.62, 0.15] } icon={myIcon}> 
-                    <Popup> Text </Popup>
-                </Marker>
+                <MyMarkersList markers={this.state.markers} />
             </Map>
         );
     }
